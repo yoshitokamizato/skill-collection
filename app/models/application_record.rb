6e6dae119ad9f5c8013a338482da80cdac157fde
MasayukiKubota <thr33rht@gmail.com>
2019-07-27 01:27:19 +0900

    Import CSV data by Rake task

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
   require 'csv'
 end
--- vendor/bundle/ruby/2.5.0/gems/ffi-1.11.1/ext/ffi_c/libffi-x86_64-darwin18/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/Users/mura/0626/skill-collection/vendor/bundle/ruby/2.5.0/gems/ffi-1.11.1/ext/ffi_c/libffi/src/x86/ffitarget.h
