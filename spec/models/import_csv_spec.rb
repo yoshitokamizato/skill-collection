7d4efe86dcf21469ef421770b9b0cd63dd064fa0
yusai13 <future.gazer7@gmail.com>
2019-08-01 22:15:04 +0900

    beforeとletを使って、コードを明示的に

--- spec/models/import_csv_spec.rb
+++ spec/models/import_csv_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe ImportCsv, type: :model do
   describe "csvのダミーデータがインポートできているかどうか" do
-    ImportCsv.import('db/csv_data/csv_data_test.csv')
-    csv_test = Text.find_by(title: "test")
+    #共通処理
+    before do
+      ImportCsv.import('db/csv_data/csv_data_test.csv')
+    end
+    let(:csv_test) { Text.find_by(title: "test") }
 
     it "titleの値が取得できているか？" do
       expect(csv_test.title).to eq "test"
