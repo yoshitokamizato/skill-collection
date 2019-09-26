f62edb9b80ec1c252f33146f1480ca046e082bec
keisukeadachi <c3a578zx@gmail.com>
2019-09-26 22:59:30 +0900

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_22_070740) do
+ActiveRecord::Schema.define(version: 2019_09_26_132818) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -66,12 +66,18 @@ ActiveRecord::Schema.define(version: 2019_09_22_070740) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "target_time", default: 0
+    t.integer "remaining"
   end
 
   create_table "texts", force: :cascade do |t|
     t.string "title"
     t.text "contents"
     t.string "genre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "twitters", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
